a9e8041e07bf1e93dc78c92216d5c32b4ad4af4e
HChilcote <94925247+HChilcote@users.noreply.github.com>
2024-03-18 08:14:18 -0400

    new main to get letter count of each letter and show report of all alphabet characters

--- main.py
+++ main.py
@@ -9,11 +9,24 @@ def num_chars_string(file_contents):
             letter_count[letter] += 1
         else:
             letter_count[letter] = 1
-    print(letter_count)
+    #print(letter_count)
     return letter_count
+
+def report_words():
+    new_count = []
+    letter_count = num_chars_string(file_contents)
+    for i in letter_count:
+        if i.isalpha():
+            new_count.append([i, letter_count[i]])
+    new_count.sort(reverse=True, key=lambda x: x[1])
+    print(f'----Start Report----')
+    for j in new_count:
+        print(f"The '{j[0]}' character showed up {j[1]} times")
+    print('-----End Report-----')
 
 def main():
     print(len(file_contents.split()))
     num_chars_string(file_contents)
-    
+    report_words()
+
 main()
