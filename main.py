c018b77a34f778ac72da0a0eff41b9ac503ba224
HChilcote <94925247+HChilcote@users.noreply.github.com>
2024-03-18 07:39:36 -0400

    updated with new function to get count of each letter in book

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 
+def num_chars_string(file_contents):
+    letter_count = {}
+    file_contents = file_contents.lower()
+    for letter in file_contents:
+        if letter in letter_count:
+            letter_count[letter] += 1
+        else:
+            letter_count[letter] = 1
+    print(letter_count)
+    return letter_count
+
 def main():
     print(len(file_contents.split()))
-
+    num_chars_string(file_contents)
+    
 main()
